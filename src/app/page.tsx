923f5094be55560bdc6d16ded98d42a2e8abc10c
prof-jeferson <jeferson.souza@ifms.edu.br>
2024-11-28 15:54:12 -0300

    Criação página reservas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,7 @@
-"use client"
-import { useState } from 'react';
-import WheelComponent from 'react-wheel-of-prizes';
-
-export default function RoletaSorteio() {
-  // Lista de alunos para o sorteio
-  const [alunos, setAlunos] = useState([
-    'Ana',
-    'Bruno',
-    'Carla',
-    'Diego',
-    'Eduarda',
-    'Felipe',
-    'Gabriela',
-    'Henrique',
-  ]);
-
-  // Estado para o nome do aluno sorteado
-  const [ganhador, setGanhador] = useState(null);
-
-  // Cores para os segmentos da roleta
-  const colors = ['#FFC107', '#03A9F4', '#4CAF50', '#E91E63', '#FFEB3B', '#9C27B0', '#FF5722', '#8BC34A'];
-
-  // Função chamada quando a roleta para
-  const handleGanhador = (ganhador) => {
-    setGanhador(ganhador);
-  };
+export default function Home() {
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-      <h1>Sorteio de Apresentação</h1>
-      
-      {/* Componente da roleta */}
-      <WheelComponent
-        segments={alunos}
-        segColors={colors}
-        winningSegment={null} // sorteia de forma aleatória
-        onFinished={(ganhador) => handleGanhador(ganhador)}
-        primaryColor="black"
-        contrastColor="white"
-        buttonText="Girar"
-        isOnlyOnce={false}
-        size={250}
-        upDuration={100}
-        downDuration={1000}
-      />
-      
-      {ganhador && (
-        <div style={{ marginTop: '20px', fontSize: '24px', fontWeight: 'bold' }}>
-          Aluno Sorteado: {ganhador}
-        </div>
-      )}
-    </div>
+    <h2>Ola</h2>
   );
 }
 
---
+++ src/app/reservas/page.tsx
@@ -0,0 +1,139 @@
+"use client"
+import Image from "next/image";
+import { stringify } from "querystring";
+import { ChangeEvent, useState } from "react";
+
+
+
+export default function Home() {
+  function getDateNow (){
+    const today = new Date()
+    return today.toISOString().split("T")[0]
+  }
+
+  const [selectedTable, setSelectedTable] = useState(null);
+  const [dateTables, setDateTables] = useState(getDateNow)
+  const tables = [{id: 1, nome: "Mesa 1"}, {id: 2, nome: "Mesa 2"}, {id: 3, nome: "Mesa 3"}]
+  const reservas = [{
+    id : 1,
+    mesa: 1,
+    data: '2024-11-29'
+  }, 
+  {
+    id : 1,
+    mesa: 2,
+    data: '2024-11-29'
+  },
+  {
+    id : 1,
+    mesa: 2,
+    data: '2024-11-28'
+  }]
+
+  function handleChangeDate (e: ChangeEvent<HTMLInputElement>) {
+    setDateTables(e.target.value)
+  }
+
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
+      
+      <div className="w-full lg:w-1/4 text-white p-4 flex items-center">
+        <div className="bg-white text-gray-800 rounded-lg shadow-lg p-4 w-full max-w-sm">
+          <img
+            src="https://github.com/MrMinerin.png"
+            alt="Usuário"
+            className="w-24 h-24 mx-auto rounded-full border-4 border-indigo-500"
+          />
+          <h2 className="text-center text-lg font-bold mt-4">Jéferson Carlos de Souza</h2>
+          <p className="text-center text-gray-600">Cliente</p>
+        </div>
+      </div>
+
+
+      <div className="w-full lg:w-1/2 bg-white p-6">
+        <div>
+          <h2 className="text-xl font-bold mb-4">Mesas Disponíveis</h2>
+          <label className="flex flex-col">
+                <input
+                  type="date"
+                  value={dateTables}
+                  min={dateTables}
+                  className="p-2 border rounded"
+                  onChange={handleChangeDate}
+
+                />
+          </label>
+        </div>
+        
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-2">
+          {tables.map((table) => {
+          if (reservas.find(reserva => dateTables === reserva.data && reserva.mesa === table.id)){
+            return (
+              <button
+                key={table.id}
+                className="p-4 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:bg-red-700"
+                onClick={() => setSelectedTable(table.nome)}
+              >
+                {table.nome}
+              </button>
+            )
+          } else {
+          return (
+            <button
+              key={table.id}
+              className="p-4 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus:bg-indigo-700"
+              onClick={() => setSelectedTable(table.nome)}
+            >
+              {table.nome}
+            </button>
+          )}})}
+        </div>
+      </div>
+
+
+      <div className="w-full lg:w-1/4 bg-gray-100 p-4 border-t lg:border-t-0 lg:border-l">
+        {selectedTable ? (
+          <div>
+            <h2 className="text-xl font-bold mb-4">Reservar {selectedTable}</h2>
+            <form className="flex flex-col space-y-4">
+              <label className="flex flex-col">
+                Nome:
+                <input
+                  type="text"
+                  className="p-2 border rounded"
+                  placeholder="Seu nome"
+                />
+              </label>
+              <label className="flex flex-col">
+                Data:
+                <input
+                  type="date"
+                  className="p-2 border rounded"
+                />
+              </label>
+              <label className="flex flex-col">
+                Pessoas:
+                <input
+                  type="number"
+                  max={4}
+                  min={1}
+                
+                  className="p-2 border rounded"
+                />
+              </label>
+              <button
+                type="submit"
+                className="bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600 focus:outline-none focus:bg-indigo-700"
+              >
+                Confirmar Reserva
+              </button>
+            </form>
+          </div>
+        ) : (
+          <p className="text-gray-700">Selecione uma mesa para reservar</p>
+        )}
+      </div>
+    </div>
+  );
+}
